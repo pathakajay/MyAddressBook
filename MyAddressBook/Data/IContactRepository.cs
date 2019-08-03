09ca8010f2cbcfba319b3481f824f7e9f92067ff
Ajay Pathak <APathak2@slb.com>
2019-08-03 06:26:47 +0530

    Sample App is Complete

---
+++ MyAddressBook/Data/IContactRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using MyAddressBook.Models;
+
+namespace MyAddressBook.Data
+{
+    public interface IContactRepository
+    {
+        Contact GetContact(int id);
+
+        int AddContact(Contact contact);
+
+        bool DeleteContact(int id);
+
+        List<Contact> GetContacts();
+    }
+}
